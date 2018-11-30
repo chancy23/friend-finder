00d5b5eac336a72b49b78a7c25297a771da27967
Chancy <chancyleath@hotmail.com>
2018-11-30 14:03:43 -0700

    added video demo to readme, refactored js code

--- ReadMe.md
+++ ReadMe.md
@@ -5,8 +5,12 @@ This application takes in user inputs (via a 10 question survey) and finds their
 
 ### Technology Used
   * Node.js and Express.js (for the routing and data storage)
-  * HTML, CSS (including Bootstrap and Font Awesome)
+  * HTML, CSS (including Bootstrap and Font Awesome Icons)
   * GitHub (repository)
   * Heroku (deployment/hosting)
+
+Deployed At: https://friend-finder102918.herokuapp.com/
+
+Video Demo: https://drive.google.com/open?id=1OhOkBei4z8MQKB0puBY3lakyQq3-Pmyl
 
 Contact chancyleath@hotmail.com if you have issues or questions
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 //dependencies
 var express = require("express");
 
-//this is now in the htmlroutes.js file, it says "path" is not defined otherwise
-//var path = require("path");
-
 //set up express for app
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -12,11 +9,10 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//get the html routes and the api routes from the files so that our frontend will work
+//get the html routes and the api routes from the files
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-//set server to listen ============================
 app.listen(PORT, function(){
     console.log("App listening on Port " + PORT);
 });
